a16cb4ecbaee97624a99e4308f3c9a01997b11cf
Juho Faarinen <juho.faarinen@gmail.com>
2021-09-10 15:32:28 +0300

    ...Teht10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+    const [repositories, setRepositories] = useState();
+    const [loading, setLoading] = useState(false);
+    const result = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' });
+
+    const fetchRepositories = async () => {
+        setLoading(true);
+        if (result.data) {
+            console.log(result.data);
+            setRepositories(result.data.repositories);
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchRepositories();
+    }, [result]);
+
+    return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
